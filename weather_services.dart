36a6644b96c4b7362fdd4bdfc77534c845bf9f26
mayuraj01 <166938302+mayuraj01@users.noreply.github.com>
2025-02-04 22:51:27 +0530

    weather_services.dart

--- weather_services.dart
+++ weather_services.dart
@@ -4,7 +4,7 @@ import 'package:weather_app/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherServices {
-  final String apiKey = '85aa4846ece99105b5e02c57575257cf';
+  final String apiKey = 'Your API Key';
 
   Future<Weather> fetchWeather(String cityName) async{
     final url =Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey');
